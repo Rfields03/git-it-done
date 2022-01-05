37cfaa7472e7ab9110a77390757c7c34e93ed4f7
Robert Field <rfields03@gmail.com>
2022-01-05 15:28:32 -0500

    update files

--- .homepage.js
+++
@@ -1,9 +0,0 @@
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
-  var response = fetch("https://api.github.com/users/octocat/repos").then(function(response))
-  console.log("inside", response);
-});
-
-console.log("outside");
-
-getUserRepos();
